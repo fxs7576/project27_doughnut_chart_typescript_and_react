aec4a52900fa031f1125f463c33cd682740d2632
Felix Surjadjaja <felix.surjadjaja@symend.com>
2022-09-21 13:05:38 -0700

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import ReactDOM from "react-dom/client";
+import ComponentChartDoughnut from "./component/doughnut/component";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById("root")!;  // getting a reference to the div with ID root
+const root = ReactDOM.createRoot(container!);  // telling React to take control of that element
+
+const App = () => {  // creating a component
+    return (
+        <ComponentChartDoughnut />
+
+    );
+};
+
+root.render(<App />);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+
+
+
+const path = require("path");
+const CompressionPlugin = require("compression-webpack-plugin")
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ["ts-loader"],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    plugins: [
+        new CompressionPlugin({
+            test: /\.js(\?.*)?$/i,
+            filename: "bundle.js",
+            algorithm: "gzip",
+            deleteOriginalAssets: false,
+        }),
+    ]
+};
